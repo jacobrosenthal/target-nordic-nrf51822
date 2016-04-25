2b5c3c7778858751a2237137ecd0de2916483334
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-04-25 15:55:00 -0400

    take other manifest fields as optional command line arguments

--- nordic-nrf51822-gcc/scripts/generate_dat.py
+++ nordic-nrf51822-gcc/scripts/generate_dat.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 
 # genereate .dat and .zip for an application binary
-# todo, take softdevice and bootloaders and do whatever alterations to the hex necesary 
 # https://github.com/NordicSemiconductor/nRF-Master-Control-Panel/blob/master/init%20packet%20handling/How%20to%20generate%20the%20INIT%20file%20for%20DFU.pdf
-# code shamelessly stolen from https://devzone.nordicsemi.com/question/22586/anyone-do-successfully-dfu-ota-for-sdk701-softdevice-s110-v710/
 
 
 import argparse, sys, zipfile, glob, os
 
+### shamelessly stolen from https://devzone.nordicsemi.com/question/22586/anyone-do-successfully-dfu-ota-for-sdk701-softdevice-s110-v710/
 def calc_crc16(binfile):
   crc = 0xFFFF
 
@@ -23,21 +22,23 @@ def convert_uint16_to_array(value):
     """ Convert a number into an array of 2 bytes (LSB). """
     return [(value >> 0 & 0xFF), (value >> 8 & 0xFF)]
 
-def _create_init_packet(init_packet):
-    hardware_version = [0xFF, 0xFF]
-    hardware_revision = [0xFF, 0xFF]
-    application_version = [0xFF, 0xFF, 0xFF, 0xFF]
-    softdevice_len = [0x01, 0x00]
-    softdevice_array = [0xFE, 0xFF]
+def _create_init_packet(init_packet, device_type, device_revision, application_version, softdevice, crc):
+    first_mask  = 0b0000000000001111
+    second_mask = 0b0000000011110000
+    third_mask  = 0b0000111100000000
+    fourth_mask = 0b1111000000000000
 
-    init_packet.extend(hardware_version)
-    init_packet.extend(hardware_revision)
-    init_packet.extend(application_version)
-    init_packet.extend(softdevice_len)
-    init_packet.extend(softdevice_array)
+    init_packet = []
+    init_packet.extend([first_mask & device_type, second_mask & device_type])
+    init_packet.extend([first_mask & device_revision, second_mask & device_revision])
+    init_packet.extend([first_mask & application_version, second_mask & application_version, third_mask & application_version, fourth_mask & application_version])
+    init_packet.extend([0x01, 0x00])
+    init_packet.extend([first_mask & softdevice, second_mask & softdevice])
+    init_packet.extend(convert_uint16_to_array(crc))
+###
 
-def _create_manifest(bin_file, dat_file, application_version, device_revision, device_type, firmware_crc16, softdevice_req):
-  manifest_text = """
+def _create_manifest(bin_file, dat_file, device_type, device_revision, application_version, softdevice, crc):
+  manifest = """
   {
     "manifest": {
         "application": {
@@ -56,12 +57,17 @@ def _create_manifest(bin_file, dat_file, application_version, device_revision, d
         "dfu_version": 0.5
     }
 }"""
-  return manifest_text % (bin_file, dat_file, application_version, device_revision, device_type, firmware_crc16, softdevice_req)
+  return manifest % (bin_file, dat_file, application_version, device_revision, device_type, crc, softdevice)
 
 def main():
 
   parser = argparse.ArgumentParser(description='generate a .bin.dat file from an input bin file.')
-  parser.add_argument ('file', metavar = 'file', type = str, help = 'The binary to crc')
+  parser.add_argument ('file', type = str, help = 'The binary to crc')
+  parser.add_argument ('--device-type', default=65535, type = int, help = 'The device type')
+  parser.add_argument ('--device-revision', default=65535, type = int, help = 'The revision')
+  parser.add_argument ('--softdevice', default=65534, type = int, help = 'The softdevice (singular)')
+  parser.add_argument ('--application-version', default=4294967295, type = int, help = 'The application version')
+
   args = parser.parse_args()
 
   basename = os.path.basename(args.file)
@@ -87,24 +93,20 @@ def main():
     archive = zipfile.ZipFile( archive_name, 'w')
 
   except:
+    bin.close();
     sys.exit('Could not open/create %s' % archive_name)
 
-  init_packet = []
-  _create_init_packet(init_packet)
-
   ba = bytearray(bin.read())
-  bin.close();
   crc = calc_crc16(ba)
-  init_packet.extend(convert_uint16_to_array(crc))
+  bin.close();
 
-  application_version = 4294967295
-  device_revision = 65535
-  device_type = 65535
-  softdevice_req = 65534
-  manifest_text = _create_manifest(basename, dat_out_name, application_version, device_revision, device_type, crc, softdevice_req)
+  init_packet = []
+  _create_init_packet(init_packet, args.device_type, args.device_revision, args.application_version, args.softdevice, crc)
+
+  manifest = _create_manifest(basename, dat_out_name, args.device_type, args.device_revision, args.application_version, args.softdevice, crc)
 
   archive.writestr(dat_out_name, buffer(bytearray(init_packet)), zipfile.ZIP_DEFLATED)
-  archive.writestr(manifest_name, manifest_text, zipfile.ZIP_DEFLATED)
+  archive.writestr(manifest_name, manifest, zipfile.ZIP_DEFLATED)
   archive.write(args.file, basename, zipfile.ZIP_DEFLATED)
   archive.close()
 
